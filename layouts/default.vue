60821ae15f684009d8ffef5419c83df6f041077d
Areola Daniel <66014556+lifeofdaniell@users.noreply.github.com>
2021-09-15 06:32:46 +0100

    init

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  head () {
+    return {
+      bodyAttrs: {
+        class: 'body'
+      }
+    }
+  },
+  mounted () {
+    /* this.$nextTick(function () {
+      this.$initWebflow()
+    }) */
+  }
+}
+</script>
